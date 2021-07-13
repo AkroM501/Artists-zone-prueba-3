3634a1f004c2fe27b6b3724e88bf856ec52c222b
iSlooZ <MIC.ESPINOZA@DUOCUC.CL>
2021-07-12 21:30:15 -0400

    Creacion de tabla Publicaciones en la BD.

--- ArtistZoneApp/models.py
+++ ArtistZoneApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Publicaciones(models.Model):
+    nombre_cuadro = models.CharField(max_length=30)
+    nombre_autor = models.CharField(max_length=50)
+    precio = models.IntegerField()
